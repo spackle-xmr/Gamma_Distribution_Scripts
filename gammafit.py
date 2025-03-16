2aa86c1fab1b9ef45dabb95c5a8e65efafbed38b
spackle-xmr <93713512+spackle-xmr@users.noreply.github.com>
2025-03-16 18:55:22 -0500

    Update gammafit.py

--- gammafit.py
+++ gammafit.py
@@ -14,9 +14,4 @@ for i in range(samplesize):
         x[i] = np.random.gamma(19.28,(1/1.61))
 
 #Fit distribution
-printy = gamma.fit(x, floc=0)
-print(printy)
-#4 gives 9.13717, 1.2944
-#4.2 gives 9.952, 1.191 with ANN Bin Class 77% 
-#5 gives 8.27, 1.426 
-#5.2 gives 8.948, 1.321
+print(gamma.fit(x, floc=0))
